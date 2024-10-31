c26b280402bf063ddde36f876b5f9cedb41d1cee
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-10-31 17:33:37 +0330

    Temporary Inter: Well, there ya go! I promised the next commit would be IR. This is a temporary IR. Some-AST, Some-HIR. Definitley will expand on both (and LIR/MIR) afterwards.

---
+++ Cephyr/Inter.ml
@@ -0,0 +1,48 @@
+module Inter = struct
+   type lvalue =
+           { lbase  : lbase
+           ; loffs  : loffs
+           }
+   and lbase =
+           | Var of variable
+           | Mem of exp
+   and loffs =
+           | NoOffs
+           | Field of field * loffs 
+           | Index of exp * loffs
+   and exp =
+           | Constant of const
+           | Lvalue of lvalue
+           | SizeOfExp of exp
+           | SizeOfType of typ
+           | AlignOfType of typ
+           | UnOp of unop * exp
+           | BinOp of binop * exp
+           | Cast of typ * exp
+           | AddrOf of lvalue
+           | StartOf of lvalue
+   and instr =
+           | Set of lvalue * exp
+           | Call of lvalue option * exp list
+           | Asm of string * lvalue list * exp list
+   and stmt =
+           | Instr of instr list
+           | Return of exp option
+           | Goto of stmt
+           | Break
+           | Continue
+           | If of exp * stmt list * stmt list
+           | Switch of exp * stmt list * stmt list
+           | Loop of stmt list
+   and typ =
+           | Void
+           | Array of typ * exp list
+           | Int of int_kind
+           | Float of float_kind
+           | Ptr of typ
+           | Fun of typ * variable list
+           | Enum of string * item list
+           | Named of string * typ
+           | Struct of string * field list
+           | Union of string * field list
+end
