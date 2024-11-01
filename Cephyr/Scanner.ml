9ca1e9da4b89c52ec138ffcd10c4158a8e252518
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-11-01 03:57:09 +0330

    Milestone for Scanner: The scanner is, more or less, ready. Of course, there will definitely changes made in the future. It may be completely overhauled. But for now, I am happy with it. Let us move on to the parser --- or another aspect of the project; for now

--- Cephyr/Scanner.ml
+++ Cephyr/Scanner.ml
@@ -9,13 +9,12 @@ module Scanner = struct
     | Identifier of string
     | Keyword of string
     | StringLiteral of string
-    | CharacterLiteral of string
+    | CharacterLiteral of char
     | EncodingPrefix of string
-    | IntegerLiteral of string
-    | HexLiteral of string
-    | BinLiteral of string
-    | OctLiteral of string
-    | BooleanLiteral of string
+    | SizeSuffix of string
+    | IntegerLiteral of int64
+    | RealLiteral of float
+    | BooleanLiteral of bool
     | LeftDelimiter of string
     | RightDelimiter of string
     | SeparatorPunctuation of string
@@ -48,6 +47,10 @@ module Scanner = struct
     let lexeme = implode_until pred scn in
     Stream.skip scn.char_stream; lexeme
 
+  let is_whitespace = function
+    | '\t' | '\space' | '\r' | '\n' -> true
+    | _ -> false
+
   let is_letter = function
     | 'a' .. 'z'
     | 'A' .. 'Z' -> true
@@ -72,7 +75,7 @@ module Scanner = struct
     | _ -> false
 
   let is_size_letter = function
-    | 'L' | 'l' | 'u' | 'U' -> true
+    | 'L' | 'l' | 'u' | 'U' | 'f' | 'F' -> true
     | _ -> false
 
   let is_identifier c =
@@ -89,6 +92,9 @@ module Scanner = struct
 
   let is_binteger c =
     is_bidigit c || is_size_letter c
+
+  let is_real_num c =
+    is_digit c || c = '.'
 
   let is_operator = function
     | '+' | '-'
@@ -124,44 +130,89 @@ module Scanner = struct
     | "for" | "typedef"
     | "_Thread_local" | "goto"
     | "union" -> Keyword lexeme
+    | "true" -> BooleanLiteral true
+    | "false" -> BooleanLiteral false
     | _ -> Identifier lexeme
+
+  let int_of_zlexeme lxm =
+    lxm |> Int64.of_string
+
+  let int_of_xlexeme lxm =
+    ("0x" ^ lxm) |> Int64.of_string
+
+  let int_of_olexeme lxm =
+    ("0o" ^ lxm) |> Int64.of_string
+
+  let int_of_blexeme lxm =
+    ("0b" ^ lxm) |> Int64.of_string
+
+  let real_of_rlexeme lxm =
+    lxm |> Float.of_string
+
+  let is_valid_end tok =
+    match tok with
+    | RightDelimiter '}' -> true
+    | _ -> false
 
   let rec scan scn =
     match Stream.peek scn.char_stream with
-    | ('L' | 'u' | 'U') as c ->
-      let demarque = Stream.npeek 2 scn.char_stream in
-      if c <> 'u' && (demarque = '"' || demarque = '\'')
-      then 
-        begin 
-          Stream.skip scn.char_stream;
-          scan (scn ++ EncodingPrefix (String.make 1 c))
-        end
-      else if c = 'u' && demarque = '8'
-      then 
-        begin 
-          let demarque' = Stream.npeek 3 scn.char_stream in
-          if (demarque' = '"') || (demarque' = '\'')
-          then 
-            begin
-              Stream.skip scn.char_stream; 
-              Stream.skip scn.char_stream;
-              scan (scn ++ EncodingPrefix "u8")
-            end
-        end
+    | '\space' | '\t' | '\r' | '\n' ->
+      Stream.drop_while is_whitespace scn.char_stream;
+      scan scn
+    | ('L' | 'u' | 'U' | 'l' | 'f') as c ->
+      begin
+        match Stream.peek_last scn.token_stream with
+        | IntegerLiteral _ | RealLiteral _ ->
+          scan (scn ++ SizeSuffix (implode_while is_size_letter scn))
+        | _ -> 
+          let demarque = Stream.peek_ahead 1 scn.char_stream in
+          match demarque with
+          | '\'' | '"' ->
+            Stream.skip scn.char_stream;
+            scan (scn ++ EncodingPrefix (String.make c))
+          | '8' ->
+            let demarque' = Stream.peek_ahead 2 scn.char_stream in
+            if demarque' = '\'' || demarque = '"'
+            then 
+              begin
+                Stream.skip scn.char_stream;
+                Stream.skip scn.char_stream;
+                scan (scn ++ EncodingPrefix "u8")
+              end
+      end
     | 'a' .. 'z' | 'A' .. 'Z' | '_' -> 
-      scan (scn ++ Identifier ((implode_while is_identifier scn) |> assess_keyword))
+      scan (scn ++ (implode_while is_identifier scn) |> assess_keyword)
     | '1' .. '9' ->
-      scan (scn ++ IntegerLiteral (implode_while is_integer scn))
-    | '+' | '-' | '>' | '<' | '=' | '*' | '%' | '/' | '&' | '|' | '!' | '~' ->
+      begin
+        let integer_lxm = Stream.take_while is_integer scn.char_stream in
+        match Stream.peek scn.char_stream with
+        | '.' ->
+          let real_left = Stream.take_while is_real_num scn.char_stream in
+          scan (scn ++ RealLiteral ((integer_lxm ^ real_left) |> real_of_rlexeme))
+        | _ ->
+          scan (scn ++ IntegerLiteral (integer_lxm |> int_of_zlexeme))
+      end
+    | '+' | '-' | '>' | '<' | '=' | '*' | '%' | '/' | '&' | '|' | '!' | '~' | '.' ->
       scan (scn ++ Operator (implode_while is_operator scn))
+    | (';' | ',' | ':' | '?') as sep ->
+      Stream.skip scn.char_stream;
+      scan (scn ++ SeparatorPunctuaction sep)
+    | ('(' | '{' | '[') as ldelim ->
+      Stream.skip scn.char_stream;
+      scan (scn ++ LeftDelimiter ldelim)
+    | (')' | '}' | ']') as rdelim ->
+      Stream.skip scn.char_stream;
+      scan (scn ++ RightDelimiter rdelim)
     | '0' ->
       begin
         Stream.skip scn.char_stream;
         let demarque = Stream.next scn.char_stream in
         match demarque with
-        | 'x' | 'X' -> (scan ++ HexLiteral (implode_while is_xinteger))
-        | 'o' | 'O' -> (scan ++ OctLiteral (implode_while is_ointeger))
-        | 'b' | 'B' -> (scan ++ BinLiteral (implode_while is_binteger))
+        | 'x' | 'X' -> (scan ++ IntegerLiteral ((implode_while is_xinteger) |> int_of_xlexeme))
+        | 'o' | 'O' -> (scan ++ IntegerLiteral ((implode_while is_ointeger) |> int_of_olexeme))
+        | 'b' | 'B' -> (scan ++ IntegerLiteral ((implode_while is_binteger) |> int_of_blexeme))
+        | '0' .. '9' 
+        | '.' ->  scan (scn ++ RealLiteral ((implode_while is_real_num) |> real_of_rlexeme))
         | _ -> raise (Wrong_symbol of (demarque, (Stream.position scn.char_stream)))
       end
     | '"' ->
