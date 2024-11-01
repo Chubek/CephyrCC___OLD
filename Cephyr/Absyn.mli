e9f764c26771603c578c08802e2311e290122c21
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-11-01 12:39:39 +0330

    Interface for AST: The interface for AST added

--- Cephyr/Absyn.mli
+++ Cephyr/Absyn.mli
@@ -2,6 +2,23 @@ module type Absyn = sig
   type t
   type top_level
   type typ
+  type typqual
+  type typkind
+  type fsig
+  type vsig
+  type strgcls
+  type idx
+  type infix_exp
+  type prefix_exp
+  type postfix_exp
+  type const_val
+  type binary_op
+  type unary_op
+  type exp
+  type rvalue
+  type init
+  type scope
+  type stmt
 
   exception No_entrypoint
 
